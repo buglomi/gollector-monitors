f2db4d89c6308a2cab9136a03a2a18b1db85d539
Erik Hollensbe <erik+github@hollensbe.org>
2014-01-04 13:18:45 -0800

    postgresql-monitor: flag parsing; baked for now.

--- src/postgresql-monitor/postgresql-monitor.go
+++ src/postgresql-monitor/postgresql-monitor.go
@@ -4,12 +4,23 @@ import (
 	"custerr"
 	"database/sql"
 	"encoding/json"
+	"flag"
 	_ "github.com/bmizerany/pq"
 	"os"
+	"strings"
 )
 
 type PGStat struct {
 	DB *sql.DB
+}
+
+var db_params = map[string][2]string{
+	"user":     [2]string{"postgres", "The username to access postgresql"},
+	"password": [2]string{"", "The password (if any) to access postgresql"},
+	"dbname":   [2]string{"template1", "The database name."},
+	"sslmode":  [2]string{"disable", "SSL Mode. Options are disable, required (no verification), and verify-full"},
+	"host":     [2]string{"localhost", "The host. Start with a / to use a unix socket"},
+	"port":     [2]string{"", "The port, only useful for TCP connections."},
 }
 
 // this gets transformed into ints based on the count of the value.
@@ -32,8 +43,16 @@ func (pg *PGStat) getCount(table_name string) int {
 	return val
 }
 
-func main() {
-	db, err := sql.Open("postgres", "user=erikh dbname=template1 sslmode=disable")
+func yield(params map[string]*string) {
+	auth_string := ""
+
+	for key, value := range params {
+		if *value != "" {
+			auth_string += key + "=" + *value + " "
+		}
+	}
+
+	db, err := sql.Open("postgres", strings.Trim(auth_string, " \t"))
 
 	if err != nil {
 		custerr.Fatal("Error connecting to postgresql database: " + err.Error())
@@ -55,4 +74,16 @@ func main() {
 
 	os.Stdout.Write(content)
 	os.Exit(0)
+}
+
+func main() {
+	params := map[string]*string{}
+
+	for key, value := range db_params {
+		params[key] = flag.String(key, value[0], value[1])
+	}
+
+	flag.Parse()
+
+	yield(params)
 }
